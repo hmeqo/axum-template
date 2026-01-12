32df1421c1a371efe46c6c346b8d29724ba86f3e
hmeqo <hmeqou@gmail.com>
2026-01-12 17:11:16 +0800

    update

--- crates/migration/src/util.rs
+++
@@ -1,8 +0,0 @@
-use sea_orm_migration::{
-    prelude::{ColumnDef, IntoIden},
-    schema::pk_auto,
-};
-
-pub fn pk_auto_bigint<T: IntoIden>(column: T) -> ColumnDef {
-    pk_auto(column).big_integer().take()
-}
